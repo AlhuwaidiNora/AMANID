b635aa0141fac3fd72c1606a1b3592b377fb59e3
NR <noura.alhuwaidi@outlook.sa>
2023-03-27 22:54:03 +0300

    First commit

---
+++ app/src/main/java/com/example/amanid/IntroOption.java
@@ -0,0 +1,11 @@
+package com.example.amanid;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+public class IntroOption extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_option);
+    }
+}
