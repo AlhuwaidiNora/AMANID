06a0d98b6a329317a7166ffef4eb6a823fd2d61c
nouratu <noura.tu19@gmail.com>
2023-03-30 05:47:58 +0300

    add get started page

---
+++ app/src/main/java/com/example/amanid/getStated_2.java
@@ -0,0 +1,14 @@
+package com.example.amanid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class getStated_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_stated2);
+    }
+}
