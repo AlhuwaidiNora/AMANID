f516a439cffb66b0e3980eeab4acb967782ebe4e
nouratu <noura.tu19@gmail.com>
2023-03-30 06:17:47 +0300

    create page 3,4 (get started)

---
+++ app/src/main/java/com/example/amanid/getStarted_page4.java
@@ -0,0 +1,14 @@
+package com.example.amanid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class getStarted_page4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_started_page4);
+    }
+}
