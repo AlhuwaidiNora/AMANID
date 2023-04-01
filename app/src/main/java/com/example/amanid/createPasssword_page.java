bd18d7ce1fc34504b0bbf1a1c56dc140cfc64ebc
nouratu <noura.tu19@gmail.com>
2023-04-01 10:02:45 +0300

    createPasssword_page + create done page + create enter id number page

---
+++ app/src/main/java/com/example/amanid/createPasssword_page.java
@@ -0,0 +1,14 @@
+package com.example.amanid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createPasssword_page extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_passsword_page);
+    }
+}
