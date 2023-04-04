fa6fb505aec176854b4eba1d3d4e0fa9b24366b0
NORA ALHUWAIDI <noura.alhuwaidi@outlook,sa>
2023-04-04 08:28:26 +0300

    intro option - coding

--- app/src/main/java/com/example/amanid/IntroOption.java
+++ app/src/main/java/com/example/amanid/IntroOption.java
@@ -1,11 +1,28 @@
 package com.example.amanid;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-public class IntroOption extends AppCompatActivity {
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.content.Intent;
+import android.os.Handler;
 
+public class IntroOption extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_intro_option_page5);
+        setContentView(R.layout.intro_page1);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(IntroOption.this, getStated_2.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 5000);
     }
 }
+
+
+
