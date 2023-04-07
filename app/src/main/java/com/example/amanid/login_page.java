f5121fbd5a2bbf48e2fafc3aebd8aaf4a9510a9c
nouratu <noura.tu19@gmail.com>
2023-04-07 10:19:31 +0300

    add if condition for password and id number in login page

--- app/src/main/java/com/example/amanid/login_page.java
+++ app/src/main/java/com/example/amanid/login_page.java
@@ -6,14 +6,61 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class login_page extends AppCompatActivity {
  Button button8;
+ EditText   editTextid_login;
+    EditText edit_pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
         button8 = findViewById(R.id.button8);
+        edit_pass = findViewById(R.id.edit_pass);
+        editTextid_login = findViewById(R.id.editTextid_login);
+        initial();
+
+        }
+
+    private void initial() {
+        button8.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           loginWithid(editTextid_login.getText().toString(), edit_pass.getText().toString());
+
+                                       }
+
+                                       private void loginWithid(String num, String pass) {
+                                           if (num.length() > 10 && pass.length() > 6) {
+
+                                           } else {
+                                               if (num.length() < 10) {
+                                                   editTextid_login.setError(" the ID number not valid");
+                                               }
+                                               if (pass.length() < 6) {
+                                                   edit_pass.setError(" the Password not valid");
+                                               }
+                                           }
+                                       }
+                                   });
+
+    }
+}
+
+
+
+
+
+
+/*
+ button8.setOnClickListener(new View.OnClickListener() {
+@Override
+public void onClick(View v) {
+        Intent intent = new Intent(login_page.this,fingerPrint_plus.class);
+        startActivity(intent);
+        }
+        });
         button8.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -21,5 +68,23 @@ public class login_page extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        button8.setOnClickListener(new View.OnClickListener() {
+                                       @Override
+                                       public void onClick(View v) {
+                                           loginWithid(editTextid_login.getText().toString(), edit_pass.getText().toString());
+                                       }
+
+                                       private void loginWithid(String num, String pass) {
+                                           if (num.length() > 10 && pass.length() > 6) {
+
+                                           }else {
+                                               if(num.length()<10){
+                                                   editTextid_login.setError(" the ID number not valid");
+                                       }
+                                               if(pass.length()<6){
+                                                   editTextid_login.setError(" the Password not valid");
+                                               }
     }
 }
+
+ */
