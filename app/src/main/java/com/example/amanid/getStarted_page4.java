0f649f7efa7219bd5b0150cc2420fac024dc5632
NORA ALHUWAIDI <noura.alhuwaidi@outlook,sa>
2023-04-07 07:44:15 +0300

    Link the buttons - 5 pages -

--- app/src/main/java/com/example/amanid/getStarted_page4.java
+++ app/src/main/java/com/example/amanid/getStarted_page4.java
@@ -1,14 +1,27 @@
 package com.example.amanid;
-
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
 
 public class getStarted_page4 extends AppCompatActivity {
+
+    Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_get_started_page4);
+
+        button4 = findViewById(R.id.button4);
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getStarted_page4.this, intro_option_page5.class);
+                startActivity(intent);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/amanid/intro_option_page5.java
@@ -0,0 +1,15 @@
+
+package com.example.amanid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class intro_option_page5 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_option_page5);
+    }
+}
+
+
