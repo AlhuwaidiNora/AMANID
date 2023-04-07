b2b8381554a92165e558ba9802eaa1952de7db38
nouratu <noura.tu19@gmail.com>
2023-04-07 09:24:58 +0300

    link the button login to login_page and link to anther pages

--- app/src/main/java/com/example/amanid/Idnumber_page6.java
+++ app/src/main/java/com/example/amanid/Idnumber_page6.java
@@ -7,14 +7,18 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+
 public class Idnumber_page6 extends AppCompatActivity {
     Button button3;
+     private EditText editTextid;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idnumber_page6);
         button3 = findViewById(R.id.button3);
+
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
--- app/src/main/java/com/example/amanid/login_page.java
+++ app/src/main/java/com/example/amanid/login_page.java
@@ -2,13 +2,24 @@ package com.example.amanid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class login_page extends AppCompatActivity {
-
+ Button button8;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_page);
+        button8 = findViewById(R.id.button8);
+        button8.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(login_page.this, fingerPrint_page7.class);
+                startActivity(intent);
+            }
+        });
     }
 }
