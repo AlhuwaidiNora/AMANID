149142f4263efe15a1fae31ab0e59339ee8d391e
nouratu <noura.tu19@gmail.com>
2023-04-11 09:33:20 +0300

    make if statement for password

--- app/src/main/java/com/example/amanid/createPasssword_page8.java
+++ app/src/main/java/com/example/amanid/createPasssword_page8.java
@@ -7,19 +7,38 @@ package com.example.amanid;
         import android.os.Bundle;
         import android.view.View;
         import android.widget.Button;
+        import android.widget.EditText;
+
 public class createPasssword_page8 extends AppCompatActivity {
     Button button5;
+    EditText editTextTextPersonName2;
+    EditText editTextTextPersonName4;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_passsword_page8);
         button5 = findViewById(R.id.button5);
+        editTextTextPersonName4 =findViewById(R.id.editTextTextPersonName4);
+        editTextTextPersonName2 =findViewById(R.id.editTextTextPersonName2);
         button5.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                    rightpass(editTextTextPersonName2.getText().toString(), editTextTextPersonName4.getText().toString());
+                }
+            private void   rightpass(String pass1, String pass2) {
+                if (pass1.length() >= 6 && pass2.length() >= 6) {
                 Intent intent = new Intent(createPasssword_page8.this, done_page9.class);
-                startActivity(intent);
-            }
-        });
+               startActivity(intent);
+
+                    } else {
+                        if (pass1.length() <6 && pass2.length() < 6) {
+                            editTextTextPersonName2.setError(" Error must enter 6 number");
+                        }
+
+                    }
+                }
+            });
+
+        }
+
     }
-}
