940bf33cc339eed463c2d870a93e077bd18e0a62
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-04-13 04:46:42 +0300

    Creating AmanID Interface

---
+++ app/src/main/java/com/example/amanid/AmanIDInterface.java
@@ -0,0 +1,14 @@
+package com.example.amanid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AmanIDInterface extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aman_idinterface);
+    }
+}
