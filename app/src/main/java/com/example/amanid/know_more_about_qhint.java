5c26476f0be97ca404cd27946a3564a8c5997365
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-04-14 10:44:43 +0300

    Add hints

--- app/src/main/java/com/example/amanid/know_more_about_qhint.java
+++ app/src/main/java/com/example/amanid/know_more_about_qhint.java
@@ -1,4 +1,6 @@
 package com.example.amanid;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,11 @@ public class know_more_about_qhint extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_know_more_about_qhint);
+
+        String[] hintQuestions = getResources().getStringArray(R.array.hint_questions);
+        Spinner hintQuestionsSpinner = findViewById(R.id.activity_know_more_about_qhint);
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, hintQuestions);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        hintQuestionsSpinner.setAdapter(adapter);
     }
 }
