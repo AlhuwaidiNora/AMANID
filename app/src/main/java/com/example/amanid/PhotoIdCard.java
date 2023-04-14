e1accc19499b4d2b2691e7ceaf61d0ecc8f6b094
Razan AlJelaify <razanfj@hotmail.com>
2023-04-14 22:14:34 +0300

    ID Card page

---
+++ app/src/main/java/com/example/amanid/PhotoIdCard.java
@@ -0,0 +1,14 @@
+package com.example.amanid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PhotoIdCard extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_photo_id_card);
+    }
+}
