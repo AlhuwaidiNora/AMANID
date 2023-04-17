b244c16e1a68c4c058b5741878b2bfffa30fa636
noura <122120860+nouratu@users.noreply.github.com>
2023-04-17 06:37:04 +0300

    fix this pleas

--- app/src/main/java/com/example/amanid/navigation_menu.java
+++ app/src/main/java/com/example/amanid/navigation_menu.java
@@ -1,14 +1,50 @@
 package com.example.amanid;
 
+import com.example.amanid.databinding.ActivityNavigationMenuBinding;
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.fragment.app.FragmentTransaction;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import  android.widget.Button;
 import android.os.Bundle;
 
 public class navigation_menu extends AppCompatActivity {
+    public ActivityNavigationMenuBinding binding;
+// test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_navigation_menu);
+        binding = ActivityNavigationMenuBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        replaceFragment(new homeFragment());
+        binding.bottomNavigationView.setOnItemSelectedListener(item -> {
+            switch(item.getItemId()){
+
+                case R.id.home:
+                    replaceFragment(new homeFragment());
+                    break;
+                case R.id.payments:
+                    replaceFragment(new paymentsFragment());
+
+                    break;
+                case R.id.transfer:
+                    replaceFragment(new transferFragment());
+
+                    break;
+                case R.id.explore:
+                    replaceFragment(new exploreFragment());
+
+                    break;
+            }
+            return true;
+        });
+    }
+    private void replaceFragment(Fragment fragment){
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.commit();
     }
 }
