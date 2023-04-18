16d7f78258aec327db23400278439c22b11b11e5
noura <122120860+nouratu@users.noreply.github.com>
2023-04-19 02:43:22 +0300

    add transfer java class  link pages  fix xml for home page + setting page

--- app/src/main/java/com/example/amanid/home_page_7.java
+++ app/src/main/java/com/example/amanid/home_page_7.java
@@ -9,7 +9,7 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 public class home_page_7 extends AppCompatActivity {
-ImageView imageview201 , imageview164;
+ImageView imageview201 , imageview164 , imageView77;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +17,7 @@ ImageView imageview201 , imageview164;
         setContentView(R.layout.activity_home_page7);
         imageview201 =findViewById(R.id.imageView210);
         imageview164 = findViewById(R.id.imageView164);
+        imageView77 = findViewById(R.id.imageView77);
 
         imageview201.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,6 +30,13 @@ ImageView imageview201 , imageview164;
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(home_page_7.this, Add_New_User.class);
+                startActivity(intent);
+            }
+        });
+        imageView77.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(home_page_7.this, transfer.class);
                 startActivity(intent);
             }
         });
---
+++ app/src/main/java/com/example/amanid/transfer.java
@@ -0,0 +1,29 @@
+package com.example.amanid;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class transfer extends AppCompatActivity {
+    Button button12 ;
+    EditText editTextamount;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.transfer);
+        button12 = findViewById(R.id.button12);
+        editTextamount = findViewById(R.id.editTextamount);
+
+        button12.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(transfer.this, home_page_7.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
