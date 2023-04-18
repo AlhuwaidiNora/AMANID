0d97e78906d0b7d943a20000799bcdd66b562c40
noura <122120860+nouratu@users.noreply.github.com>
2023-04-18 12:25:08 +0300

    Splash Screen code in SuccessfulLogin page

--- app/src/main/java/com/example/amanid/SuccessfulLogin.java
+++ app/src/main/java/com/example/amanid/SuccessfulLogin.java
@@ -1,25 +1,37 @@
 package com.example.amanid;
-
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 
+
 public class SuccessfulLogin extends AppCompatActivity {
-    Button addbutton2;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_successful_login);
-        addbutton2 =findViewById(R.id.addbutton2);
-        addbutton2.setOnClickListener(new View.OnClickListener() {
+        new Handler().postDelayed(new Runnable() {
+
+
+
             @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(SuccessfulLogin.this, home_page_7.class);
-                  startActivity(intent);
+
+            public void run() {
+
+                Intent i = new Intent(SuccessfulLogin.this, home_page_7.class);
+
+                startActivity(i);
+
+
+                finish();
+
             }
-        });
+
+        }, 1*1000); // wait for 1 seconds
+
     }
 
 
