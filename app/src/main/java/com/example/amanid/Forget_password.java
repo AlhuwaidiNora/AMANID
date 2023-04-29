45cc98349abaad62a7a3912cb1da42a4e8a7e836
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-04-29 03:25:17 +0300

    link forget  password with firebase

--- app/src/main/java/com/example/amanid/Forget_password.java
+++ app/src/main/java/com/example/amanid/Forget_password.java
@@ -2,52 +2,42 @@ package com.example.amanid;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
-import androidx.core.content.ContextCompat;
-
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.Toast;
+
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.biometric.BiometricManager;
 import androidx.biometric.BiometricPrompt;
 import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.core.content.ContextCompat;
 
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import java.util.concurrent.Executor;
-
-import java.util.concurrent.Executor;
-
-import android.content.Intent;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.ImageView;
-
 
 public class Forget_password extends AppCompatActivity {
-    BiometricPrompt biometricPrompt;
-    BiometricPrompt.PromptInfo promptInfo;
-    ConstraintLayout mMainLayout2;
-     Button confirmbutton;
-     Button button17;
+    private FirebaseAuth mAuth;
+    private BiometricPrompt biometricPrompt;
+    private BiometricPrompt.PromptInfo promptInfo;
+    private ConstraintLayout mMainLayout2;
+    private Button confirmbutton;
+    private Button button17;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_forget_password);
+
+        // Initialize Firebase Authentication
+        mAuth = FirebaseAuth.getInstance();
+
         mMainLayout2 = findViewById(R.id.mMainLayout2);
         confirmbutton =findViewById(R.id.confirmbutton);
         button17 = findViewById(R.id.button17);
@@ -66,7 +56,6 @@ public class Forget_password extends AppCompatActivity {
             }
         });
 
-
         BiometricManager biometricManager= BiometricManager.from(this);
         switch (biometricManager.canAuthenticate()){
             case BiometricManager.BIOMETRIC_ERROR_NO_HARDWARE:
@@ -74,11 +63,12 @@ public class Forget_password extends AppCompatActivity {
                 break;
             case BiometricManager.BIOMETRIC_ERROR_HW_UNAVAILABLE:
                 Toast.makeText(getApplicationContext(),"not working ",Toast.LENGTH_LONG).show();
-
-
+                break;
             case BiometricManager.BIOMETRIC_ERROR_NONE_ENROLLED:
                 Toast.makeText(getApplicationContext()," NO FingerPrint Assign ",Toast.LENGTH_LONG).show();
+                break;
         }
+
         Executor executor= ContextCompat.getMainExecutor(this);
 
         biometricPrompt= new BiometricPrompt(Forget_password.this, executor, new BiometricPrompt.AuthenticationCallback() {
@@ -87,12 +77,13 @@ public class Forget_password extends AppCompatActivity {
                 super.onAuthenticationFailed();
             }
 
-
             public void onAuthenticationSucceeded(@NonNull BiometricPrompt.AuthenticationResult result) {
                 super.onAuthenticationSucceeded(result);
                 Toast.makeText(getApplicationContext(), " Successful ", Toast.LENGTH_LONG).show();
                 mMainLayout2.setVisibility(View.VISIBLE);
+                signInWithFirebase(); // Call Firebase authentication method
             }
+
             public void onAuthenticationError(int errorCode, @NonNull CharSequence errString){
                 super.onAuthenticationError(errorCode, errString);
             }
@@ -102,14 +93,27 @@ public class Forget_password extends AppCompatActivity {
                 .setDescription("Use FingerPrint  ").setDeviceCredentialAllowed(true).build();
 
         biometricPrompt.authenticate(promptInfo);
+    }
 
-
-
-
-
+    // Firebase Authentication method
+    private void signInWithFirebase() {
+        mAuth.signInWithEmailAndPassword("user@example.com", "password123")
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(Forget_password.this, "Authentication successful.",
+                                    Toast.LENGTH_LONG).show();
+                            Intent intent = new Intent(Forget_password.this, createPasssword_later.class);
+                            startActivity(intent);
+                        } else {
+                            Toast.makeText(Forget_password.this, "Authentication failed.",
+                                    Toast.LENGTH_LONG).show();
+                        }
+                    }
+                });
     }
 }
-
-
-
 
