235ded902b22f35f2b9647c8eb58a58e10ab6f29
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-09 02:35:06 +0300

    create class that responsible for managing user session information

---
+++ app/src/main/java/com/example/amanid/UserSession.java
@@ -0,0 +1,37 @@
+package com.example.amanid;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class UserSession {
+    private SharedPreferences preferences;
+
+    private SharedPreferences.Editor editor;
+    private Context context;
+
+    public UserSession(Context context){
+        this.context=context;
+        preferences=context.getSharedPreferences("data",0);
+        editor=preferences.edit();
+    }
+
+
+
+
+
+
+
+
+    public void setUserID(String id){
+        editor.putString("id",id).commit();
+    }
+
+    public String gtUserID(){
+        return preferences.getString("id",null);
+    }
+
+
+}
