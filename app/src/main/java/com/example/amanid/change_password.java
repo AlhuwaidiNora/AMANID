1b02aacf383899e9a161ee8745b30c841782abf8
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-08 23:26:12 +0300

    add necessary methods to update firebase -change password-

--- app/src/main/java/com/example/amanid/change_password.java
+++ app/src/main/java/com/example/amanid/change_password.java
@@ -15,6 +15,9 @@ import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.EmailAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -79,14 +82,13 @@ public class change_password extends AppCompatActivity {
             }
         });
 
-// ...
-
         button232.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("Button", "Button Clicked");
                 String pass = newPasswordEditText.getText().toString();
                 String pass2 = confirmPasswordEditText.getText().toString();
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
 
                 // Check if the new password matches the confirm password
                 if (!pass.equals(pass2)) {
@@ -94,45 +96,38 @@ public class change_password extends AppCompatActivity {
                     return;
                 }
 
-                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-                if (user != null) {
-                    // User is authenticated
-                    String userId = user.getUid();
-                    DatabaseReference databaseReference = FirebaseDatabase.getInstance().getReference("users").child(userId);
+                // Update the password using Firebase Authentication
+                user.updatePassword(pass)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if (task.isSuccessful()) {
+                                    // Password updated successfully
+                                    Toast.makeText(change_password.this, "Password updated successfully", Toast.LENGTH_SHORT).show();
 
-                    // Update the password in the database directly with the new password
-                    databaseReference.child("pass").setValue(pass).addOnCompleteListener(new OnCompleteListener<Void>() {
-                        @Override
-                        public void onComplete(@NonNull Task<Void> task) {
-                            if (task.isSuccessful()) {
-                                // Password updated successfully in the database
-                                // Now update the password in Firebase Authentication
-                                user.updatePassword(pass).addOnCompleteListener(new OnCompleteListener<Void>() {
-                                    @Override
-                                    public void onComplete(@NonNull Task<Void> task) {
-                                        if (task.isSuccessful()) {
-                                            // Password updated successfully in Firebase Authentication system
-                                            Toast.makeText(change_password.this, "Password updated successfully", Toast.LENGTH_SHORT).show();
-
-                                            Intent intent = new Intent(change_password.this, setting.class);
-                                            startActivity(intent);
-                                            finish();
-                                        } else {
-                                            // Password update failed in Firebase Authentication system
-                                            Toast.makeText(change_password.this, "Failed to update password", Toast.LENGTH_SHORT).show();
-                                        }
-                                    }
-                                });
-                            } else {
-                                // Password update failed in the database
-                                Toast.makeText(change_password.this, "Failed to update password in database", Toast.LENGTH_SHORT).show();
+                                    // Reauthenticate the user with the new password
+                                    AuthCredential credential = EmailAuthProvider.getCredential(user.getEmail(), pass);
+                                    user.reauthenticate(credential)
+                                            .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                                @Override
+                                                public void onComplete(@NonNull Task<Void> task) {
+                                                    if (task.isSuccessful()) {
+                                                        // User reauthenticated successfully
+                                                        // Proceed with further actions if needed
+                                                    } else {
+                                                        // Failed to reauthenticate the user
+                                                        // Handle the failure or prompt the user to sign in again
+                                                    }
+                                                }
+                                            });
+                                } else {
+                                    // An error occurred while updating the password
+                                    Toast.makeText(change_password.this, "Failed to update password", Toast.LENGTH_SHORT).show();
+                                }
                             }
-                        }
-                    });
-                } else {
-                    // User is not authenticated
-                    Toast.makeText(change_password.this, "User not authenticated", Toast.LENGTH_SHORT).show();
-                }
+                        });
             }
         });
-    }}
+
+
+}}
