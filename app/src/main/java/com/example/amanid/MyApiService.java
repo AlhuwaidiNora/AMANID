8ce28ef10f9333e95c58bf7da2f501647851de36
Nora Alhuwaidi <noura.alhuwaidi@outlook.sa>
2023-05-08 11:16:23 +0300

    inshilize Retrofit

---
+++ app/src/main/java/com/example/amanid/MyApiService.java
@@ -0,0 +1,12 @@
+package com.example.amanid;
+
+import com.google.firebase.firestore.DocumentSnapshot;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.http.GET;
+
+public interface MyApiService {
+    @GET("/users")
+    Call<List<DocumentSnapshot>> getUsers();
+}
