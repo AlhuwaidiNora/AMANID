28193995bfccf9ac6e563dd3e3095b308ac3b6d7
Razan AlJelaify <razanfj@hotmail.com>
2023-05-12 14:32:44 +0300

    Add the new SIGN IN page

---
+++ app/src/main/java/com/example/amanid/new_SIGNIN.java
@@ -0,0 +1,14 @@
+package com.example.amanid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class new_SIGNIN extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_signin);
+    }
+}
