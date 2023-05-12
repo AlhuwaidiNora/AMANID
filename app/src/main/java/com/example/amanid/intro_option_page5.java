591f16abba970430ba5ab72eed24371e67781d56
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-12 16:02:16 +0300

    link intro, intro option

--- app/src/main/java/com/example/amanid/intro_option_page5.java
+++ app/src/main/java/com/example/amanid/intro_option_page5.java
@@ -11,19 +11,18 @@ import android.widget.ImageView;
 
 public class intro_option_page5 extends AppCompatActivity {
 
+Button button22, button7;
 
-
-    ImageView imageView35 , imageView38;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro_option_page1);
 
-        imageView35 = findViewById(R.id.imageView35);
-        imageView38 = findViewById(R.id.imageView38);
+        button22 = findViewById(R.id.button22);
+        button7 = findViewById(R.id.button7);
 
-        imageView35.setOnClickListener(new View.OnClickListener() {
+        button22.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(intro_option_page5.this, getStarted_page2.class);
@@ -31,7 +30,7 @@ public class intro_option_page5 extends AppCompatActivity {
             }
         });
 
-        imageView38.setOnClickListener(new View.OnClickListener() {
+        button7.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(intro_option_page5.this, login_page.class);
