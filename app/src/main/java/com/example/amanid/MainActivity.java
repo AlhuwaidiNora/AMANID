31d00b1fee2555090256a858eb5cbbbf884d7d3e
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-12 21:19:59 +0300

    wrtie methoud - API sumilator

--- app/src/main/java/com/example/amanid/MainActivity.java
+++ app/src/main/java/com/example/amanid/MainActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -18,11 +19,17 @@ import com.google.firebase.database.FirebaseDatabase;
 public class MainActivity extends AppCompatActivity {
 
     private static final long DELAY = 2000;
+    private String receiver;
+    private double amount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_page1);
+        receiver=getIntent().getStringExtra("receiver");
+        amount=getIntent().getDoubleExtra("amount",0);
+        Log.d("SSSS", "onCreate: "+getIntent().getStringExtra("receiver"));
+        Log.d("SSSS", "onCreate: "+getIntent().getDoubleExtra("amount",0));
 
         // Initialize Firebase
         FirebaseDatabase database = FirebaseDatabase.getInstance();
@@ -44,6 +51,8 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void run() {
                 Intent intent = new Intent(MainActivity.this, intro_option_page5.class);
+                intent.putExtra("amount",amount);
+                intent.putExtra("receiver",receiver);
                 startActivity(intent);
                 finish();
             }
