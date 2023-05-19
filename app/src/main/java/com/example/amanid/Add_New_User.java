d3c5fbc0b7f5a1358e18c4ac024b198b2633a345
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-19 06:44:34 +0300

    established code whisper using AWS CodeWhisperer

--- app/src/main/java/com/example/amanid/Add_New_User.java
+++ app/src/main/java/com/example/amanid/Add_New_User.java
@@ -1,18 +1,29 @@
 package com.example.amanid;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.Bucket;
+import software.amazon.awssdk.services.s3.model.ListBucketsResponse;
+
 public class Add_New_User extends AppCompatActivity {
- Button button20;
+
+    Button button20;
+    S3Client s3Client;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_new_user);
+
         button20 = findViewById(R.id.button20);
         button20.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -21,5 +32,20 @@ public class Add_New_User extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        s3Client = S3Client.builder()
+                .region(Region.US_EAST_1)
+                .build();
+
+        listBuckets();
+    }
+
+    private void listBuckets() {
+        ListBucketsResponse response = s3Client.listBuckets();
+        List<Bucket> buckets = response.buckets();
+
+        for (Bucket bucket : buckets) {
+            System.out.println(bucket.name());
+        }
     }
 }
