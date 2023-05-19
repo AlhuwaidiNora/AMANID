0f615b22af9f02d6eb411510b6f617280434add0
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-19 04:44:49 +0300

    desgin new wallet according last changes

--- app/src/main/java/com/example/amanid/wallet.java
+++ app/src/main/java/com/example/amanid/wallet.java
@@ -12,7 +12,7 @@ import android.widget.ImageView;
 
 public class wallet extends AppCompatActivity {
 
-    ImageView imageView15, imageView164;
+    ImageView imageView15;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +20,6 @@ public class wallet extends AppCompatActivity {
         setContentView(R.layout.activity_wallet);
         imageView15 = findViewById(R.id.imageView15);
 
-        imageView164 = findViewById(R.id.imageView164);
 
         imageView15.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,33 +29,6 @@ public class wallet extends AppCompatActivity {
             }
         });
 
-        imageView164.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                // Handle the click event here
-                // You can show a dialog, start a new activity, or perform any other action
-                // to display the options "Add money from credit card" and "Add money from bank transfer"
 
-                // Example: Show a dialog with the two options
-                AlertDialog.Builder builder = new AlertDialog.Builder(wallet.this);
-                builder.setTitle("Add Money");
-                builder.setItems(new CharSequence[]{"Add money from credit card", "Add money from bank transfer"},
-                        new DialogInterface.OnClickListener() {
-                            @Override
-                            public void onClick(DialogInterface dialog, int which) {
-                                // Handle the option click event here
-                                if (which == 0) {
-                                    // Option 1: Add money from credit card
-                                    // Perform the corresponding action
-                                } else if (which == 1) {
-                                    // Option 2: Add money from bank transfer
-                                    // Perform the corresponding action
-                                }
-                            }
-                        });
-                builder.show();
-            }
-
-        });
     }
 }
