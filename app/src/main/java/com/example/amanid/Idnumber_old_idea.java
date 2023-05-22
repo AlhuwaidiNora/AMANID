4548a137e69523050f32b8e968389cf0e47b9be0
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-22 22:27:33 +0300

    method extraction (validateIdNumber().),variable declaration for Button, EditText, and FirebaseAuth were declared as private,,code reordering

--- app/src/main/java/com/example/amanid/Idnumber_old_idea.java
+++ app/src/main/java/com/example/amanid/Idnumber_old_idea.java
@@ -1,18 +1,17 @@
 package com.example.amanid;
 
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.google.firebase.auth.FirebaseAuth;
 
 public class Idnumber_old_idea extends AppCompatActivity {
-    Button button3;
+    private Button button3;
     private EditText editTextid;
     private FirebaseAuth mAuth;
 
@@ -20,27 +19,27 @@ public class Idnumber_old_idea extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_idnumber_old_idea);
-        button3 = findViewById(R.id.button3);
-        editTextid = findViewById(R.id.editTextid);
+        initializeViews();
         mAuth = FirebaseAuth.getInstance();
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                rightid(editTextid.getText().toString());
-
+                validateIdNumber(editTextid.getText().toString());
             }
-            private void  rightid(String num) {
-                if (num.length() >= 10 ) {
+
+            private void validateIdNumber(String num) {
+                if (num.length() >= 10) {
                     Intent intent = new Intent(Idnumber_old_idea.this, fingerPrint_page7_later.class);
                     startActivity(intent);
                 } else {
-                    if (num.length() < 10) {
-                        editTextid.setError(" the ID number not valid");
-                    }
-
+                    editTextid.setError("The ID number is not valid");
                 }
             }
         });
+    }
 
+    private void initializeViews() {
+        button3 = findViewById(R.id.button3);
+        editTextid = findViewById(R.id.editTextid);
     }
 }
