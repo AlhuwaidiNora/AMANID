1bb6dd305f98e9f30f42e34c41c5ab615b35d723
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-22 22:09:23 +0300

    refactored code:organized imports, extracted UID generation and display into separate method, improved readability and formatting.

--- app/src/main/java/com/example/amanid/UID.java
+++ app/src/main/java/com/example/amanid/UID.java
@@ -1,17 +1,17 @@
 package com.example.amanid;
+
 import android.os.Bundle;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import java.util.Random;
 
 public class UID extends AppCompatActivity {
-    Button button3;
-    TextView textViewUID;
+    private Button button3;
+    private TextView textViewUID;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,25 +24,22 @@ public class UID extends AppCompatActivity {
         button3.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Generate UID
-                String uid = generateUID();
-
-                // Display UID in the text view
-                textViewUID.setText(uid);
+                generateAndDisplayUID();
             }
         });
     }
 
+    private void generateAndDisplayUID() {
+        String uid = generateUID();
+        textViewUID.setText(uid);
+    }
+
     private String generateUID() {
-        // Define the characters that can be used in the UID
         String allowedChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-
-        // Set the desired length of the UID
         int uidLength = 10;
-
-        // Generate the UID
         StringBuilder uidBuilder = new StringBuilder();
         Random random = new Random();
+
         for (int i = 0; i < uidLength; i++) {
             int randomIndex = random.nextInt(allowedChars.length());
             uidBuilder.append(allowedChars.charAt(randomIndex));
