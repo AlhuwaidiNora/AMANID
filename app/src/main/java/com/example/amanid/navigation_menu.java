7bd21dbb49093b70e7c1a8fb6e441fba81f0ff70
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-22 22:43:28 +0300

    MVC pattern ,separation of concerns,code reusability,error handling

--- app/src/main/java/com/example/amanid/navigation_menu.java
+++ app/src/main/java/com/example/amanid/navigation_menu.java
@@ -1,53 +1,53 @@
 package com.example.amanid;
 
-import com.example.amanid.databinding.ActivityNavigationMenuBinding;
-
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentTransaction;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import  android.widget.Button;
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.amanid.databinding.ActivityNavigationMenuBinding;
 
 public class navigation_menu extends AppCompatActivity {
     public ActivityNavigationMenuBinding binding;
-// test
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_navigation_menu);
         binding = ActivityNavigationMenuBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
-        replaceFragment(new homeFragment());
+
+        setupNavigation();
+    }
+
+    private void setupNavigation() {
         binding.bottomNavigationView.setOnItemSelectedListener(item -> {
             switch(item.getItemId()){
-
                 case R.id.home:
                     replaceFragment(new homeFragment());
                     break;
                 case R.id.payments:
                     replaceFragment(new paymentsFragment());
-
                     break;
                 case R.id.transfer:
                     replaceFragment(new transferFragment());
-
                     break;
                 case R.id.explore:
                     replaceFragment(new exploreFragment());
-
                     break;
             }
             return true;
         });
+
+        // Set the initial fragment
+        replaceFragment(new homeFragment());
     }
-    private void replaceFragment(Fragment fragment){
+
+    private void replaceFragment(Fragment fragment) {
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
-        fragmentTransaction.replace(R.id.frameLayout,fragment);
+        fragmentTransaction.replace(R.id.frameLayout, fragment);
         fragmentTransaction.commit();
     }
 }
-
