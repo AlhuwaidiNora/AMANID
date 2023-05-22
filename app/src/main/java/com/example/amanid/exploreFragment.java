1c6c570ef9fa10f3d0ed59dc2bda77d4596d77f8
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-22 22:36:04 +0300

    some minor improvements for clarity and consistency

--- app/src/main/java/com/example/amanid/exploreFragment.java
+++ app/src/main/java/com/example/amanid/exploreFragment.java
@@ -1,26 +1,16 @@
 package com.example.amanid;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link exploreFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
-public class exploreFragment extends Fragment {
+import androidx.fragment.app.Fragment;
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+public class exploreFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
@@ -28,15 +18,6 @@ public class exploreFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment exploreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static exploreFragment newInstance(String param1, String param2) {
         exploreFragment fragment = new exploreFragment();
         Bundle args = new Bundle();
@@ -56,9 +37,7 @@ public class exploreFragment extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_explore, container, false);
     }
 }
