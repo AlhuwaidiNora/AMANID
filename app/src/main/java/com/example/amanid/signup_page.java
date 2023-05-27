53bc922efdca7369b9b777ef0c249b8a2bf664ad
Nora Alhuwaidi <Noura.alhuwaidi@outlook.sa>
2023-05-27 12:47:06 +0300

    add validation for checkBox8 in button9 onClick listener

--- app/src/main/java/com/example/amanid/signup_page.java
+++ app/src/main/java/com/example/amanid/signup_page.java
@@ -1,21 +1,19 @@
 package com.example.amanid;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.util.Log;
-import android.widget.Spinner;
-import android.widget.ArrayAdapter;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ArrayAdapter;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,6 +26,7 @@ original code
 public class signup_page extends AppCompatActivity {
     EditText editTextid_signup , editTextpass ,editTextpass2 ,editTextid_qhint;
     TextView  textView29;
+    CheckBox checkBox3;
     private String editTextValue;
     Button button9 ;
     EditText hintAnswerEditText;
@@ -65,7 +64,7 @@ public class signup_page extends AppCompatActivity {
         // editTextid_qhint = findViewById(R.id. hintAnswerEditText);
         hintAnswerEditText = findViewById(R.id.hintAnswerEditText);
         button9 = findViewById(R.id.button9);
-
+          checkBox3 = findViewById(R.id.checkBox3);
         textView29.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -73,8 +72,13 @@ public class signup_page extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-        button9.setOnClickListener( new View.OnClickListener(){
+        button9.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
+                if (!checkBox3.isChecked()) {
+                    Toast.makeText(signup_page.this, "Agreement not checked", Toast.LENGTH_LONG).show();
+                    return; // Exit the method if checkbox is not checked
+                }
+
                 database = FirebaseDatabase.getInstance();
                 reference = database.getReference("users");
 
@@ -82,6 +86,7 @@ public class signup_page extends AppCompatActivity {
                 String pass = editTextpass.getText().toString();
                 String pass2 = editTextpass2.getText().toString();
                 String qhint = hintAnswerEditText.getText().toString();
+
                 if (idnum.isEmpty() || qhint.isEmpty() || pass.isEmpty() || pass2.isEmpty()) {
                     Toast.makeText(signup_page.this, "Please fill all fields", Toast.LENGTH_LONG).show();
                 } else if (!pass.equals(pass2)) {
@@ -93,25 +98,31 @@ public class signup_page extends AppCompatActivity {
                         @Override
                         public void onDataChange(@NonNull DataSnapshot snapshot) {
                             // check if id not registered before
-                            if (snapshot.exists()){
-                                Toast.makeText(signup_page.this, " ID number is already signup", Toast.LENGTH_LONG).show();
-                            }else{
+                            if (snapshot.exists()) {
+                                Toast.makeText(signup_page.this, "ID number is already signed up", Toast.LENGTH_LONG).show();
+                            } else {
                                 // sending data
-                                HelperClass helperClass = new HelperClass(idnum, pass, pass2,qhint);
+                                HelperClass helperClass = new HelperClass(idnum, pass, pass2, qhint);
                                 reference.child(idnum).setValue(helperClass);
                                 new UserSession(signup_page.this).setUserID(idnum);
                                 reference.child(idnum).child("idnum").setValue(idnum);
                                 reference.child("users").child(idnum).child("hint").setValue(qhint);
 
-                                Toast.makeText(signup_page.this, "you have signup successfully!", Toast.LENGTH_LONG).show();
+                                Toast.makeText(signup_page.this, "You have signed up successfully!", Toast.LENGTH_LONG).show();
                                 finish();
                                 Intent intent = new Intent(signup_page.this, fingerPrint_page7_later.class);
-                                startActivity(intent);}}
+                                startActivity(intent);
+                            }
+                        }
+
                         @Override
                         public void onCancelled(@NonNull DatabaseError error) {
-                        }});
+                        }
+                    });
                 }
-            }});
+            }
+        });
+
 
     }
 
